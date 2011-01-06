154464a49808e348968a3f3a18602ebd13993ea6
Erik Scott <erik@newlevelstudio.com>
2011-01-05 18:25:31 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gosocial::Application.config.secret_token = '7a37447689cd05d87dd43511be9c53395d1df5c48145267a14a569c667f101e79c668d0252f2a6e2c37778dd00f9708100719cb3f540e5c4f6fc1682be0b097c'
